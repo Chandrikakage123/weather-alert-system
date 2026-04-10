bbc929c471a24e654def91bf0a1e7a8cc74fc889
Chandrikakage123 <155279866+Chandrikakage123@users.noreply.github.com>
2026-04-10 12:06:37 +0530

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,17 +1,38 @@
 import requests
 
 def check_weather():
-    url = "https://api.open-meteo.com/v1/forecast?latitude=28.61&longitude=77.20&current_weather=true"
+    url = "https://api.open-meteo.com/v1/forecast?latitude=28.61&longitude=77.20&current_weather=true&hourly=relativehumidity_2m,windspeed_10m"
     data = requests.get(url).json()
 
+    # Temperature
     temp = data["current_weather"]["temperature"]
 
-    print(f"Current Temperature: {temp}")
+    # Wind
+    wind = data["current_weather"]["windspeed"]
 
+    # Humidity (first value from hourly)
+    humidity = data["hourly"]["relativehumidity_2m"][0]
+
+    print(f"🌡️ Temperature: {temp}°C")
+    print(f"💧 Humidity: {humidity}%")
+    print(f"🌪️ Wind Speed: {wind} km/h\n")
+
+    # Alerts
     if temp > 35:
         print("🔥 Heat Alert!")
-    else:
+
+    if humidity > 80:
+        print("💧 High Humidity Alert!")
+
+    if wind > 40:
+        print("🌪️ Strong Wind Alert!")
+
+    if humidity > 70:
+        print("🌧️ Possible Rain Alert!")
+
+    if temp <= 35 and humidity <= 70 and wind <= 40:
         print("✅ Weather Normal")
+
 
 if __name__ == "__main__":
     check_weather()
